4ebe72317bd16b9e3b2626dd4ebd8838512951a4
Vaios Chrsitodoulou <vallos1501@gmail.com>
2023-11-25 12:36:18 +0200

    implemented pca

--- training_cifar10_cnn.py
+++ training_cifar10_cnn.py
@@ -3,38 +3,42 @@ import tensorflow as tf
 from keras import layers, models
 from keras.datasets import cifar10 as cifar10
 import matplotlib.pyplot as plt
+from sklearn.decomposition import PCA
+import numpy as np
+
+EPOCHS = 10
 
 # Initialize the neptune environment
 NEPTUNE_API_TOKEN = "eyJhcGlfYWRkcmVzcyI6Imh0dHBzOi8vYXBwLm5lcHR1bmUuYWkiLCJhcGlfdXJsIjoiaHR0cHM6Ly9hcHAubmVwdHVuZS5haSIsImFwaV9rZXkiOiJlMDQwYzVmYy01NTU5LTQ2NmMtOTQ3Ny00OTc5NWUyZjA1NmQifQ=="
 NEPTUNE_PROJECT = "vaioschr/NN-DL-assignment"
 run = neptune.init_run(project=NEPTUNE_PROJECT, api_token=NEPTUNE_API_TOKEN)
 
-params = {
-    "max_epochs": 10,
-    "optimizer": "Adam",
-}
-run["parameters"] = params
-
-# Load CIFAR-10 training and test data
+# Load CIFAR-10 dataset
 (train_images, train_labels), (test_images, test_labels) = cifar10.load_data()
 
-# Normalize pixel values to be between 0 and 1
-train_images, test_images = train_images / 255.0, test_images / 255.0
+# Reshape images to 1D array
+train_images_flat = train_images.reshape(train_images.shape[0], -1)
+test_images_flat = test_images.reshape(test_images.shape[0], -1)
 
-class_names = ['airplane','automobile','bird','cat','deer','dog','frog','horse','ship','truck']
+# Normalize pixel values to between 0 and 1
+train_images_flat = train_images_flat.astype('float32') / 255.0
+test_images_flat = test_images_flat.astype('float32') / 255.0
 
-# plt.figure(figsize=(10,10))
-# for i in range(25):
-#     plt.subplot(5,5,i+1)
-#     plt.xticks([])
-#     plt.yticks([])
-#     plt.grid(False)
-#     plt.imshow(train_images[i])
-#     # The CIFAR labels happen to be arrays, 
-#     # which is why you need the extra index
-#     plt.xlabel(class_names[train_labels[i][0]])
-# plt.show()
+# Apply PCA
+n_components = 100  # Adjust the number of components as needed
+pca = PCA(n_components=n_components)
+train_images_pca = pca.fit_transform(train_images_flat)
+test_images_pca = pca.transform(test_images_flat)
 
+# Reconstruct the images
+train_images_reconstructed = pca.inverse_transform(train_images_pca)
+test_images_reconstructed = pca.inverse_transform(test_images_pca)
+
+# Reshape the reconstructed images to original shape
+train_images_reconstructed = train_images_reconstructed.reshape(train_images.shape)
+test_images_reconstructed = test_images_reconstructed.reshape(test_images.shape)
+
+# Initialize the model
 model = models.Sequential()
 model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)))
 model.add(layers.MaxPooling2D((2, 2)))
@@ -42,20 +46,23 @@ model.add(layers.Conv2D(64, (3, 3), activation='relu'))
 model.add(layers.MaxPooling2D((2, 2)))
 model.add(layers.Conv2D(64, (3, 3), activation='relu'))
 
-model.summary()
-
 model.add(layers.Flatten())
 model.add(layers.Dense(64, activation='relu'))
 model.add(layers.Dense(10))
 
-model.summary()
-
 model.compile(optimizer='adam', loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True), metrics=['accuracy'])
 
-history = model.fit(train_images, train_labels, epochs=10, validation_data=(test_images, test_labels))
+model.summary()
+
+# Train the model with the transformed data
+history = model.fit(train_images_reconstructed, train_labels, epochs=EPOCHS, validation_data=(test_images_reconstructed, test_labels))
 
 # Log Hyperparameters
-run['parameters'] = {'epochs': 10}
+params = {
+    "max_epochs": EPOCHS,
+    "optimizer": "Adam",
+}
+run["parameters"] = params
 
 # Log Model Summary
 run['model_summary'] = str(model.summary())
@@ -67,7 +74,7 @@ for epoch in range(10):
     run['val/loss'].log(history.history['val_loss'][epoch])
     run['val/accuracy'].log(history.history['val_accuracy'][epoch])
 
-test_loss, test_acc = model.evaluate(test_images, test_labels, verbose=2)
+test_loss, test_acc = model.evaluate(test_images_reconstructed, test_labels, verbose=2)
 
 run['test_loss'] = test_loss
 run['test_accuracy'] = test_acc
@@ -75,11 +82,4 @@ run['test_accuracy'] = test_acc
 # Complete Neptune Experiment
 run.stop()
 
-# plt.show(history.history['accuracy'], label='accuracy')
-# plt.plot(history.history['val_accuracy'], label = 'val_accuracy')
-# plt.xlabel('Epoch')
-# plt.ylabel('Accuracy')
-# plt.ylim([0.5, 1])
-# plt.legend(loc='lower right')
-
-# print(test_acc)
+print(test_acc)
