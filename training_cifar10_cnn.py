d4a56fe291014d17d8ba34f140ef9ff846948346
Vaios Chrsitodoulou <vallos1501@gmail.com>
2023-11-25 18:10:17 +0200

    cleanup

--- training_cifar10_cnn.py
+++ training_cifar10_cnn.py
@@ -3,17 +3,12 @@ import tensorflow as tf
 from keras import layers, models
 from keras.datasets import cifar10 as cifar10
 import matplotlib.pyplot as plt
+import time
 
-# Initialize the neptune environment
+# Define parameters
 NEPTUNE_API_TOKEN = "eyJhcGlfYWRkcmVzcyI6Imh0dHBzOi8vYXBwLm5lcHR1bmUuYWkiLCJhcGlfdXJsIjoiaHR0cHM6Ly9hcHAubmVwdHVuZS5haSIsImFwaV9rZXkiOiJlMDQwYzVmYy01NTU5LTQ2NmMtOTQ3Ny00OTc5NWUyZjA1NmQifQ=="
 NEPTUNE_PROJECT = "vaioschr/NN-DL-assignment"
-run = neptune.init_run(project=NEPTUNE_PROJECT, api_token=NEPTUNE_API_TOKEN)
-
-params = {
-    "max_epochs": 10,
-    "optimizer": "Adam",
-}
-run["parameters"] = params
+EPOCHS = 10
 
 # Load CIFAR-10 training and test data
 (train_images, train_labels), (test_images, test_labels) = cifar10.load_data()
@@ -21,20 +16,10 @@ run["parameters"] = params
 # Normalize pixel values to be between 0 and 1
 train_images, test_images = train_images / 255.0, test_images / 255.0
 
-class_names = ['airplane','automobile','bird','cat','deer','dog','frog','horse','ship','truck']
+# Initialize Neptune
+run = neptune.init_run(project=NEPTUNE_PROJECT, api_token=NEPTUNE_API_TOKEN)
 
-# plt.figure(figsize=(10,10))
-# for i in range(25):
-#     plt.subplot(5,5,i+1)
-#     plt.xticks([])
-#     plt.yticks([])
-#     plt.grid(False)
-#     plt.imshow(train_images[i])
-#     # The CIFAR labels happen to be arrays, 
-#     # which is why you need the extra index
-#     plt.xlabel(class_names[train_labels[i][0]])
-# plt.show()
-
+# Configure the model and it's layers
 model = models.Sequential()
 model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)))
 model.add(layers.MaxPooling2D((2, 2)))
@@ -42,20 +27,28 @@ model.add(layers.Conv2D(64, (3, 3), activation='relu'))
 model.add(layers.MaxPooling2D((2, 2)))
 model.add(layers.Conv2D(64, (3, 3), activation='relu'))
 
-model.summary()
-
 model.add(layers.Flatten())
 model.add(layers.Dense(64, activation='relu'))
 model.add(layers.Dense(10))
 
-model.summary()
-
 model.compile(optimizer='adam', loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True), metrics=['accuracy'])
 
-history = model.fit(train_images, train_labels, epochs=10, validation_data=(test_images, test_labels))
+model.summary()
+
+start_time = time.time()
+
+# Train the model
+history = model.fit(train_images, train_labels, epochs=EPOCHS, validation_data=(test_images, test_labels))
+
+end_time = time.time()
 
 # Log Hyperparameters
-run['parameters'] = {'epochs': 10}
+run['parameters'] = {
+    'epochs': EPOCHS,
+    'optimizer': 'adam',
+    'loss': 'SparseCategoricalCrossentropy',
+    'duration': end_time - start_time,
+}
 
 # Log Model Summary
 run['model_summary'] = str(model.summary())
@@ -75,11 +68,4 @@ run['test_accuracy'] = test_acc
 # Complete Neptune Experiment
 run.stop()
 
-# plt.show(history.history['accuracy'], label='accuracy')
-# plt.plot(history.history['val_accuracy'], label = 'val_accuracy')
-# plt.xlabel('Epoch')
-# plt.ylabel('Accuracy')
-# plt.ylim([0.5, 1])
-# plt.legend(loc='lower right')
-
-# print(test_acc)
+print(test_acc)
